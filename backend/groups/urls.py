9d5e1f5bae88b8bdaa9cff3414b822b1027d7060
Muchael123 <bathenglok@gmail.com>
2023-10-09 18:29:59 +0300

    Group workings added

--- backend/groups/urls.py
+++ backend/groups/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('creategroup/',views.create_group),
     path('groups/',views.groups),
-    path('member/add/',views.add_members),
+    path('member/add/<str:groupid>/',views.add_members),
     path('groups/<str:id>/',views.group_members)
 ]
--- backend/groups/views.py
+++ backend/groups/views.py
@@ -1,19 +1,42 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+import pyrebase
+from decouple import config
+import requests
 from .models import Group,Members
 from .serialisers import GroupSerializer,MemberSerializer
+
+FIREBASE_CONFIG = {
+    "apiKey": config('FIREBASE_API_KEY'),
+    "authDomain": config('FIREBASE_AUTH_DOMAIN'),
+    "databaseURL": config('FIREBASE_DATABASE_URL'),
+    "projectId": config('FIREBASE_PROJECT_ID'),
+    "storageBucket": config('FIREBASE_STORAGE_BUCKET'),
+    "messagingSenderId": config('FIREBASE_MESSAGING_SENDER_ID'),
+    "appId": config('FIREBASE_APP_ID'),
+    "measurementId": config('FIREBASE_MEASUREMENT_ID'),
+}
+firebase = pyrebase.initialize_app(FIREBASE_CONFIG)
+
+storage = firebase.storage()
+database = firebase.database()
 
 # Create your views here.
 @api_view(["POST"])
 def create_group(request):
-    data = request.data
-    serialiser = GroupSerializer(data=data)
-    if serialiser.is_valid():
-        serialiser.save()
-        return Response({"mess":"group created successfully"})
-    return Response(serialiser.errors)
-
+    try:
+        name = request.data["name"]
+        print(name)
+        groups_ref = database.child("Groups")
+        data = {"name": name}
+        new_group_ref = groups_ref.push(data)
+        group_id = new_group_ref["name"]
+        return Response({"name": name, "id" : group_id})
+    except requests.exceptions.HTTPError as error:
+            print(error)
+            error_message = error["error"]["message"]
+            print(error_message)
+            return Response({"mess":error})
 @api_view(["GET"])
 def groups(request):
     data = Group.objects.all()
@@ -21,26 +44,30 @@ def groups(request):
     return Response(serializer.data)
 
 @api_view(["POST"])
-def add_members(request):
+def add_members(request,groupid):
     data = request.data
-    serialiser = MemberSerializer(data=data)
-    if serialiser.is_valid():
-        serialiser.save()
-        return Response({"message":"member added successfully"})
-    return Response(serialiser.errors)
+    group_ref = database.child("Groups").child(groupid).child("members")
+    members = {
+        "phone":data['phone'],
+        "amount": data["amount"],
+        "interval": data["interval"]
+    }
+    group_ref.push(members)
+    
+    return Response(members)
 
 @api_view(["GET"])
 def group_members(request,id):
-    try:
-        members = Members.objects.filter(group= id)
-        group = Group.objects.get(groupid = id)
-    except:
-        return Response({"message":"group does not exist"})
-    ser = GroupSerializer(group)
-    serialiser = MemberSerializer(members,many=True)
+    # try:
+    #     members = Members.objects.filter(group= id)
+    #     group = Group.objects.get(groupid = id)
+    # except :
+    #     return Response({"message":"group does not exist"})
+    # ser = GroupSerializer(group)
+    # serialiser = MemberSerializer(members,many=True)
     response = {
         "groupname":ser.data["groupname"],
         "members": serialiser.data
-        
     }
+    # }
     return Response(response)
